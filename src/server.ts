07cf6f4663f71b6ab3cc165fa3478b94d3eb2ffc
Marcelino Eilert <marcelinoeilert@MacBook-Air-de-Marcelino.local>
2020-04-27 17:33:36 -0400

    start point: arquivosTS

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World 2' }));
+app.use(routes);
+
+// app.get('/', (request, response) => response.json({ message: 'Hello World 2' }));
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
