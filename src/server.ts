494ade60bbb77e5fd3a9e6c2cc96f9d3849472a8
Marcelino Eilert <marcelinoeilert@MacBook-Air-de-Marcelino.local>
2020-04-27 17:24:35 -0400

    start point: eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express, { request } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World 2'})
-})
+app.get('/', (request, response) => response.json({ message: 'Hello World 2' }));
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
