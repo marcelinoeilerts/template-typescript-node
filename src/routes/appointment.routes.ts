57a20c0a3e1397f297c455da263fac27aae0bef9
Marcelino Eilert <marcelinoeilert@MacBook-Air-de-Marcelino.local>
2020-04-28 17:38:38 -0400

    feature/cadastroAgendamentos => validando date com date-fns

--- src/routes/appointment.routes.ts
+++ src/routes/appointment.routes.ts
@@ -1,17 +1,41 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+/**
+ * parseISO -> converte string para date nativo do js Date()
+ * startOfHour -> pega hora e coloca no comeco dela 13:43:23 => 13:00:00
+ */
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/users', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
